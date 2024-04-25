d22cccd84155e4d263f5fdf8f9078ebbedd02eac
Arsen Sydoryk <arsen.sydoryk@gmail.com>
2024-04-26 00:50:35 +0300

    LAB-4: Init lab4

---
+++ Race/Assets/Scripts/RoadController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class RoadController : MonoBehaviour
+    {
+        private float _length;
+        private float _partLength;
+        private Coroutine _coroutine;
+
+        [SerializeField] private float speed;
+
+        private void Start()
+        {
+            _length = transform.localScale.x;
+            _partLength = _length / 2;
+            ManageCoroutine(true);
+        }
+
+        private void ManageCoroutine(bool start)
+        {
+            if (start)
+            {
+                _coroutine ??= StartCoroutine(MoveRoad());
+            }
+            else
+            {
+                if (_coroutine != null)
+                {
+                    StopCoroutine(_coroutine);
+                    _coroutine = null;
+                }
+            }
+        }
+
+        private IEnumerator MoveRoad()
+        {
+            while (true)
+            {
+                var currentPos = transform.position;
+                transform.position = new Vector3(currentPos.x + (Time.deltaTime * speed), currentPos.y, currentPos.z);
+                yield return null;
+            }
+        }
+
+        public void Move(Vector3 position)
+        {
+            var currentPos = transform.position;
+            transform.position = new Vector3(position.x - _partLength, currentPos.y, currentPos.z);
+        }
+
+        public Vector3 GetEndPosition()
+        {
+            var currentPos = transform.position;
+            return new Vector3(currentPos.x - _partLength, currentPos.y, currentPos.z);
+        }
+
+        public Vector3 GetStartPosition()
+        {
+            var currentPos = transform.position;
+            return new Vector3(currentPos.x + _partLength, currentPos.y, currentPos.z);
+        }
+    }
+}
---
+++ Race/Assets/Scripts/RoadsController.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace DefaultNamespace
+{
+    public class RoadsController : MonoBehaviour
+    {
+        [SerializeField] private RoadController firstRoad;
+        [SerializeField] private RoadController secondRoad;
+
+        [FormerlySerializedAs("endPosition")] [SerializeField]
+        private Transform endTransform;
+
+        private Coroutine _coroutine;
+
+        private void Start()
+        {
+            ManageCoroutine(true);
+        }
+
+        private void ManageCoroutine(bool start)
+        {
+            if (start)
+            {
+                _coroutine ??= StartCoroutine(MoveRoads());
+            }
+            else
+            {
+                if (_coroutine != null)
+                {
+                    StopCoroutine(_coroutine);
+                    _coroutine = null;
+                }
+            }
+        }
+
+        private IEnumerator MoveRoads()
+        {
+            while (true)
+            {
+                ComparePositions(firstRoad, secondRoad);
+                ComparePositions(secondRoad, firstRoad);
+                yield return null;
+            }
+        }
+
+        private void ComparePositions(RoadController fRoad, RoadController sRoad)
+        {
+            var roadPos = fRoad.GetEndPosition();
+            if (endTransform.position.x < roadPos.x)
+            {
+                fRoad.Move(sRoad.GetEndPosition());
+            }
+        }
+
+
+        private void Update()
+        {
+        }
+    }
+}
